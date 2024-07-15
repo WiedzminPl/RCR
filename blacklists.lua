8c34ea60ef4cddea31699bf497b2889fc2eab7ac
Ghost <93557531+WiedzminPl@users.noreply.github.com>
2024-07-16 00:49:28 +0200

    Create rcr_blacklists.lua

---
+++ blacklists.lua
@@ -0,0 +1,13 @@
+local reports = {
+  degeneracy = {  
+    00000000, -- user id of (guy who does not respect the age of consent)
+  },
+  cybercrime = {  
+    00000000, -- user id of (leaker)
+  },
+  
+  miscellaneous = {  
+    00000000, -- user id of (guy who does not like 0,0,0)
+    
+  },
+}
